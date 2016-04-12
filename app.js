0401fd67d3029efee7c153c5655ec13c0be872bf
Himanshu Jha <himanshujha19@gmail.com>
2016-04-11 23:54:03 -0700

    Adding Twitter node logic for reading from tweets and inserting to database

--- app.js
+++ app.js
@@ -1,13 +1,70 @@
+natural = require('natural')
+Twitter = require('twitter-node-client').Twitter;
 database = require('./database')
 
-database.connectToDatabase(function(db) {
-    database.insert(db,"node", function(err, result) {
-        if (err) {
-            console.log(err)
-        } else if (result == "success") {
-            console.log("Word successfully added to database")
+var config = {
+    "consumerKey": process.env.CONSUMER_KEY,
+    "consumerSecret": process.env.CONSUMER_SECRET,
+    "accessToken": process.env.ACCESS_TOKEN,
+    "accessTokenSecret": process.env.ACCESS_TOKEN_SECRET,
+    "callBackUrl": ""
+}
+
+var twitter = new Twitter(config);
+var myDB
+
+var error = function (err, response, body) {
+    //console.log('ERROR [%s]', err);
+};
+
+var timelineSuccess = function (data) {
+    var text = ""
+    res = JSON.parse(data);
+    res.forEach(function(tweet) {
+//        parseTweet(tweet.text);
+        if (text == "") {
+            text = tweet.text
         } else {
-            console.log("result %s", result)
+            text = text + " " + tweet.text
         }
     });
+    parseTweet(text)
+};
+
+function parseTweet(tweet) {
+    tokenizer = new natural.WordTokenizer();
+    var wordArray = tokenizer.tokenize(tweet)
+    for (int i = 0; i < wordArray.length; i++) {
+    wordArray.forEach(function(word) {
+        database.insert(myDB, word, function(err, result) {
+            if (err) {
+                console.log(err)
+            } else if (result == "success") {          
+                console.log("Word successfully added to database")
+            } else {
+                console.log("result %s", result)
+            }
+        });
+    });
+};
+
+connect()
+twitter.getUserTimeline({'screen_name' : "jay_s_h", 'count' : 2}, error, timelineSuccess);
+
+
+function connect() {
+    database.connectToDatabase(function(db) {
+        myDB = db
+    });
+}
+/*
+database.insert(db,"node", function(err, result) {
+    if (err) {
+        console.log(err)
+    } else if (result == "success") {
+        console.log("Word successfully added to database")
+    } else {
+        console.log("result %s", result)
+    }
 });
+*/
