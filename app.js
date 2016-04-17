e0637551e8dd52d38b190da02c629c1eae39fd31
Himanshu Jha <himanshujha19@gmail.com>
2016-04-17 16:42:11 -0700

    Changing the implementation of how a tweet is parsed

--- app.js
+++ app.js
@@ -13,15 +13,24 @@ var config = {
 var twitter = new Twitter(config);
 var myDB
 
+/*
+    Callback function for error when retrieving user timeline
+*/
 var error = function (err, response, body) {
-    //console.log('ERROR [%s]', err);
+    console.log('ERROR [%s]', err);
 };
 
+/*
+    The callback function when the timeline is successfully retrieved.
+    Combines the tweet into one single space-separated string value,
+    which is then parsed.
+
+    TODO :: Instead of combining all the tweets, parse it individually?
+*/
 var timelineSuccess = function (data) {
     var text = ""
     res = JSON.parse(data);
     res.forEach(function(tweet) {
-//        parseTweet(tweet.text);
         if (text == "") {
             text = tweet.text
         } else {
@@ -31,39 +40,27 @@ var timelineSuccess = function (data) {
     parseTweet(text)
 };
 
+/*
+    Function that parses the text into individual words (token) and
+    adds to database.
+*/
 function parseTweet(tweet) {
     tokenizer = new natural.WordTokenizer();
     var wordArray = tokenizer.tokenize(tweet)
-    wordArray.forEach(function(word) {
-        database.insert(myDB, word, function(err, result) {
-            if (err) {
-                console.log(err)
-            } else if (result == "success") {
-                console.log("Word successfully added to database")
-            } else {
-                console.log("result %s", result)
-            }
-        });
+    database.insert(myDB, wordArray, function(err, result) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("succcess ", result.upsertedCount)
+        }
     });
 };
 
 connect()
 twitter.getUserTimeline({'screen_name' : "jay_s_h", 'count' : 2}, error, timelineSuccess);
 
-
 function connect() {
     database.connectToDatabase(function(db) {
         myDB = db
     });
 }
-/*
-database.insert(db,"node", function(err, result) {
-    if (err) {
-        console.log(err)
-    } else if (result == "success") {
-        console.log("Word successfully added to database")
-    } else {
-        console.log("result %s", result)
-    }
-});
-*/
