342a4c05a6cb5c395f7ef10c4a7f4ddb5f4660ea
Himanshu Jha <himanshujha19@gmail.com>
2016-09-05 18:57:53 -0700

    Adding angular functionality to the app

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('twitter-miner', []);
+
+app.controller('InputController', ['$scope', function($scope) {
+    $scope.twitterHandle = ""
+    $scope.numOfTweets = 0;
+    
+    $scope.submit = function() {
+        
+    }
+}]);
